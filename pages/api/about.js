da41373358b0cace4ac49b0e4d21bf06797fe075
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-17 13:28:03 +0000

    defining components.

---
+++ pages/api/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, officiis
+        est maiores deleniti eum laudantium ad consequuntur provident doloribus
+        enim!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, officiis
+        est maiores deleniti eum laudantium ad consequuntur provident doloribus
+        enim!
+      </p>
+    </div>
+  );
+};
+
+export default About;
