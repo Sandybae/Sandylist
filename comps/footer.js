de31a50433950c70b9060025890484eecee05700
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-17 13:28:54 +0000

    defining other components.

---
+++ comps/footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Copyright 2023 Sandy List</div>;
+};
+
+export default Footer;
---
+++ pages/sandy/index.js
@@ -0,0 +1,9 @@
+const Queens = () => {
+  return (
+    <div>
+      <h1>All Queens</h1>
+    </div>
+  );
+};
+
+export default Queens;
