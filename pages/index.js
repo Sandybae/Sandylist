6117f01642e34a8f82e85d66d45f1833011ec6e9
Sandra Yeboah <sandraeyeboah@gmail.com>
2023-02-17 14:33:09 +0000

    defining layout component.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 import Navbar from "@/comps/Navbar";
 import Footer from "@/comps/footer";
-import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, nihil.
@@ -26,7 +22,6 @@ export default function Home() {
         voluptatum aliquid.
       </p>
       <Link href="/sandy">See Queens Listing</Link>
-      <Footer />
     </div>
   );
 }
